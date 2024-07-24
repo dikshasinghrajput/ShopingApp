f711fe9854c649b9af672a7cde1c4c83f2741e2e
Dhiraj Lochib <dhirajch145@gmail.com>
2024-07-24 18:01:49 +0530

    1:41:22

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/widget/support_widget.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,9 +13,33 @@ class _MyWidgetState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(children: [
-          Image.asset("images/login.png")
-        ],),
+        margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/login.png"),
+            Center(
+              child: Text(
+                "Sign In",
+                style: AppWidget.semiboldTextFieldStyle(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Please enter the details below to\n                     continue.",
+              style: AppWidget.lightTextFieldStyle(),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Email",
+              style: AppWidget.semiboldTextFieldStyle(),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Color(0xFFF4F5F9),borderRadius: BorderRadius.circular(20)),
+              child: TextField(decoration: InputDecoration(border: InputBorder.none),),
+            ),
+          ],
+        ),
       ),
     );
   }
