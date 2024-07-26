ba72e6063ceae9b2e42564851642254c093f138a
Dhiraj Lochib <dhirajch145@gmail.com>
2024-07-26 17:48:19 +0530

    error

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/pages/signup.dart';
 import 'package:shoppingapp/widget/support_widget.dart';
 
 class Login extends StatefulWidget {
@@ -12,33 +13,112 @@ class _MyWidgetState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset("images/login.png"),
-            Center(
-              child: Text(
-                "Sign In",
-                style: AppWidget.semiboldTextFieldStyle(),
-              ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(
+                top: 40.0, left: 20.0, right: 20.0, bottom: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("images/login.png"),
+                Center(
+                  child: Text(
+                    "Sign In",
+                    style: AppWidget.semiboldTextFieldStyle(),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Please enter the details below to\n                     continue.",
+                  style: AppWidget.lightTextFieldStyle(),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Email",
+                  style: AppWidget.semiboldTextFieldStyle(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF4F5F9),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "rajput@gmail.com"),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Password",
+                  style: AppWidget.semiboldTextFieldStyle(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF4F5F9),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "12345678"),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    padding: EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don,t have an account? ",
+                        style: AppWidget.lightTextFieldStyle()),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Signup()),);
+                      },
+                      child: Text(
+                        "SignUp",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 20.0),
-            Text(
-              "Please enter the details below to\n                     continue.",
-              style: AppWidget.lightTextFieldStyle(),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              "Email",
-              style: AppWidget.semiboldTextFieldStyle(),
-            ),
-            Container(
-              decoration: BoxDecoration(color: Color(0xFFF4F5F9),borderRadius: BorderRadius.circular(20)),
-              child: TextField(decoration: InputDecoration(border: InputBorder.none),),
-            ),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/pages/signup.dart
@@ -0,0 +1,170 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shoppingapp/pages/login.dart';
+import 'package:shoppingapp/widget/support_widget.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  String? name, email, password;
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController mailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+  registration() async {
+    if (password != null && name != null && email != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email!, password: password!);
+      } catch (e) {}
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(
+                top: 40.0, left: 20.0, right: 20.0, bottom: 40.0),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset("images/login.png"),
+                  Center(
+                    child: Text(
+                      "Sign Up",
+                      style: AppWidget.semiboldTextFieldStyle(),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  Text(
+                    "Please enter the details below to\n                     continue.",
+                    style: AppWidget.lightTextFieldStyle(),
+                  ),
+                  SizedBox(height: 15.0),
+                  Text(
+                    "Name",
+                    style: AppWidget.semiboldTextFieldStyle(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF4F5F9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter your Name';
+                        }
+                        return null;
+                      },
+                      controller: namecontroller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "rajput"),
+                    ),
+                  ),
+                  Text(
+                    "Email",
+                    style: AppWidget.semiboldTextFieldStyle(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF4F5F9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter your  Email';
+                        }
+                        return null;
+                      },
+                      controller: mailcontroller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "rajput@gmail.com"),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  Text(
+                    "Password",
+                    style: AppWidget.semiboldTextFieldStyle(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF4F5F9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter your Password';
+                        }
+                        return null;
+                      },
+                      controller: passwordcontroller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "12345678"),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Center(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "SIGN UP",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Already have an account? ",
+                          style: AppWidget.lightTextFieldStyle()),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        child: Text(
+                          "SignIn",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
