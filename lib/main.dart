14c197d2efe367dbd2c645f32480da1e5315525c
dikshasinghrajput <dikshatouchwood@gmail.com>
2024-07-30 21:08:13 +0530

    faltu

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shoppingapp/pages/login.dart';
 
-import 'package:shoppingapp/pages/product_details.dart';
-import 'package:shoppingapp/pages/signup.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyBPD3xaleuO6OpwgnCQG8HQiaafJLXXuEw',
+      appId: '1:865499570129:android:2ea73ca99dc265c533caf0',
+      messagingSenderId: 'messagingSenderId',
+      projectId: 'shopapp-84a94',
+    ),
+  );
   runApp(const MyApp());
 }
 
